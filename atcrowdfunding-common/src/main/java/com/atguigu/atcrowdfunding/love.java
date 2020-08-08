f199d4f26ebd55ff4029799a8016c8f48915edc9
tuyue <1315379336@qq.com>
2020-08-08 16:33:21 +0800

    新增love类

---
+++ atcrowdfunding-common/src/main/java/com/atguigu/atcrowdfunding/love.java
@@ -0,0 +1,13 @@
+package com.atguigu.atcrowdfunding;
+
+/**
+ * @PROJECT_NAME: atcrowdfunding-parent
+ * @DESCRIPTION:
+ * @author: yue
+ * @DATE: 2020/8/8 16:32
+ */
+public class love {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
